8db055e236667e34e031409b4aa3bcda2652a01f
dinhanhtuan22 <dinhanhtuan22@gmail.com>
2021-12-06 00:04:52 +0700

    create application structure

---
+++ eShopSolution.Application/Catalog/Products/IManageProductService.cs
@@ -0,0 +1,18 @@
+﻿using eShopSolution.Application.Catalog.Products.Dtos;
+using eShopSolution.Application.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eShopSolution.Application.Catalog.Products
+{
+    public interface IManageProductService
+    {
+        Task<int> Create(ProductCreateRequest request); //trả về mã sản phẩm vừa tạo
+        Task<int> Update(ProductEditRequest request);
+        Task<int> Delete(int productId);
+        Task<List<ProductViewModel>> GetAll();
+        Task<PagedViewModel<ProductViewModel>> GetAllPaging(string keyword, int pageIndex, int pageSize);
+    }
+}
